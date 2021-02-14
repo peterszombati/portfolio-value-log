929f51804e2f6f32b93ff7dfa3fd37c69b1cd823
Peter Szombati <peter.szombati@bvtech.io>
2021-02-14 18:12:02 +0100

    update package.json; create index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {BlockchainHandler} from './typescript/BlockchainHandler'
+import {createCashHistory} from './typescript/createCashHistory'
+import {createTransactions} from './typescript/createTransactions'
+import {sha256hash} from './typescript/utils/sha256hash'
+
+export {BlockchainHandler, createCashHistory, createTransactions, sha256hash}
