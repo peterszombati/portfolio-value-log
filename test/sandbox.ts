620dd94a1643b89bbc34ba9929da007ac87f3e31
Peter Szombati <peter.szombati@bvtech.io>
2021-02-14 18:06:43 +0100

    update sandbox.ts

--- test/sandbox.ts
+++ test/sandbox.ts
@@ -1,5 +1,22 @@
+import {createCashHistory} from "../src/typescript/createCashHistory";
 
 describe('test', () => {
   it('test', () => {
+    createCashHistory(['log/demo/cash']).push('cash', {
+      dataFormatId: 0,
+      executedAtDate: new Date().toISOString(),
+      accountId: 0,
+      type: 'deposit',
+      amount: 0,
+      currency: 'HUF',
+      exchangedTo: '',
+      receivedAmount: 0,
+      receivedAccountId: 0,
+      commissionAmount: 0,
+      commissionCurrency: '',
+      taxAmount: 0,
+      taxCurrency: '',
+      isin: ''
+    })
   })
 })
